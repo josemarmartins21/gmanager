445c7ba88e48687fabe7c8fb5a7cb264ea9085d4
josemarmartins21 <josemar21@outlook.pt>
2026-08-16 22:50:07 +0100

    feat(products): add input validation for products store 2

--- serve/app/Http/Controllers/ProductController.php
+++ serve/app/Http/Controllers/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ProductRequest;
 use App\Models\Product;
 use App\Services\products\contracts\ProductInterface;
 use Illuminate\Http\Request;
@@ -41,9 +42,16 @@ class ProductController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(ProductRequest $request)
     {
-        //
+        try {
+            //$request->validated();
+            
+        } catch (\Throwable $th) {
+            return response()->json([
+                'message' => $th->getMessage(),
+            ], 500);
+        }
     }
 
     /**
