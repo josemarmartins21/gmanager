c775dc42a845b96b4a8adf574c3eda9e0913b904
josemarmartins21 <josemar21@outlook.pt>
2026-09-12 07:34:46 +0100

    feat: btn de marcar venda como paga criado e add alguns ajustes de estilos

--- serve/routes/web.php
+++ serve/routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\MarkSaleAsPaidController;
 use App\Http\Controllers\PdfController;
 use App\Http\Controllers\PermissionController;
 use App\Http\Controllers\ProductController;
@@ -14,6 +15,9 @@ use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth')->group(function() {
+    Route::patch('sale-status/{sale}', MarkSaleAsPaidController::class)->name('sale-status')
+    ->middleware('can:alterar status da venda');
+
     Route::resource('categories', CategoryController::class);
     Route::resource('products', ProductController::class);
     Route::resource('stock-movements', StockMovementController::class)->except(['show']);
